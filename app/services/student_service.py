aaa50ed04a0ccd2b425e6cfc072439be9b3adf49
Monirul <monirul429588@gmail.com>
2026-06-17 15:20:50 +0600

    Fix Fetch Student

--- app/services/student_service.py
+++ app/services/student_service.py
@@ -2,13 +2,29 @@ from sqlalchemy.orm import Session
 from app.models.student import Student
 
 
-def create_student(db: Session, student_id: int, name: str):
+def create_student(db: Session, student_id, name):
+    existing = db.query(Student).filter(Student.student_id == student_id).first()
+
+    if existing:
+        return existing  # prevent crash
+
     student = Student(student_id=student_id, name=name)
     db.add(student)
     db.commit()
     db.refresh(student)
-    return student
+    return {
+    "student_id": student.student_id,
+    "name": student.name
+}
 
 
 def get_students(db: Session):
-    return db.query(Student).all()
+    students = db.query(Student).all()
+
+    return [
+        {
+            "student_id": s.student_id,
+            "name": s.name
+        }
+        for s in students
+    ]
